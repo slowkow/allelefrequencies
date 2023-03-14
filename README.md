649da7e8faa066e104864519a694c8524fd2372b
Kamil Slowikowski <kslowikowski@gmail.com>
2023-03-14 23:54:42 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,56 +1,123 @@
----
-title: "Allele Frequencies Net Database"
-author: "Kamil Slowikowski"
-date: "2023-03-14"
-output: github_document
----
+Allele Frequencies Net Database
+================
+Kamil Slowikowski
+2023-03-14
 
-# Allele Frequencies Net Database
+## Introduction
 
-Here, we share a script `allelefrequencies.py` that downloads allele
-frequencies for HLA, KIR, MIC, and cytokine genes from this website:
+Here, we share a script `allelefrequencies.py` to download allele
+frequencies for HLA, KIR, MIC, and cytokine genes from [Allele Frequency
+Net Database](http://allelefrequencies.net) (AFND).
 
-- http://allelefrequencies.net
-
-Then, the script writes .tsv files like this:
-
-```
-ls allelefrequencies.net/hla/
-A.tsv
-B.tsv
-C.tsv
-DPA1.tsv
-DPB1.tsv
-DQA1.tsv
-DQB1.tsv
-DRB1.tsv
-```
+We also share [57 TSV files](allelefrequencies.net/) ready for analysis.
 
 Each file looks like this:
 
-```
-head allelefrequencies.net/A.tsv
-
-allele   population                               allele_freq  sample_size
-A*01:01  Argentina Rosario Toba                   0.0760       86
-A*01:01  Armenia combined Regions                 0.1250       100
-A*01:01  Australia Cape York Peninsula Aborigine  0.0530       103
-A*01:01  Australia Groote Eylandt Aborigine       0.0270       75
-A*01:01  Australia New South Wales Caucasian      0.1870       134
-A*01:01  Australia Yuendumu Aborigine             0.0080       191
-A*01:01  Austria                                  0.1460       200
-A*01:01  Azores Central Islands                   0.0800       59
-A*01:01  Azores Oriental Islands                  0.1150       43
+``` r
+d <- fread("allelefrequencies.net/hla/A.tsv")
+head(d)
 ```
 
-Please cite the latest manuscript about Allele Frequency Net Database:
+    ##     allele                              population indivs_over_n alleles_over_2n   n
+    ## 1: A*01:01                  Argentina Rosario Toba          15.1          0.0760  86
+    ## 2: A*01:01                Armenia combined Regions                        0.1250 100
+    ## 3: A*01:01 Australia Cape York Peninsula Aborigine                        0.0530 103
+    ## 4: A*01:01      Australia Groote Eylandt Aborigine                        0.0270  75
+    ## 5: A*01:01     Australia New South Wales Caucasian                        0.1870 134
+    ## 6: A*01:01            Australia Yuendumu Aborigine                        0.0080 191
 
-Gonzalez-Galarza FF, McCabe A, Santos EJMD, Jones J, Takeshita L, Ortega-Rivera
-ND, et al. [Allele frequency net database (AFND) 2020 update: gold-standard
-data classification, open access genotype data and new query
-tools.](https://pubmed.ncbi.nlm.nih.gov/31722398) Nucleic Acids Res. 2020;48:
-D783–D788. doi:10.1093/nar/gkz1029
+Definitions:
 
-And thanks to David A. Wells for sharing
-[scrapeAF](https://github.com/DAWells/scrapeAF), which I forked to create this
-script.
+    alleles_over_2n (Alleles / 2n)
+    Allele Frequency: total number of copies of the allele in the population sample
+    in three decimal format.
+    
+    indivs_over_n (Individuals / n)
+    Percentage of individuals who have the allele or gene.
+
+## Examples
+
+Here are a few examples of how we can use R to analyze these data.
+
+View the largest and smallest populations available in the data:
+
+``` r
+d %>%
+  mutate(n = parse_number(n)) %>%
+  select(population, n) %>%
+  unique() %>%
+  arrange(-n)
+```
+
+    ##                                    population       n
+    ##   1:             Germany DKMS - German donors 3456066
+    ##   2:              USA NMDP European Caucasian 1242890
+    ##   3:          USA NMDP African American pop 2  416581
+    ##   4:              USA NMDP Mexican or Chicano  261235
+    ##   5:              USA NMDP South Asian Indian  185391
+    ##  ---                                                 
+    ## 436:                          Bulgaria Romani      13
+    ## 437:                            Cameroon Sawa      13
+    ## 438:    Paraguay/Argentina Ache NA-DHS_24 (G)      13
+    ## 439:                      Cameroon Baka Pygmy      10
+    ## 440: Paraguay/Argentina Guarani NA-DHS_23 (G)      10
+
+Sum the allele frequencies for each population and see if they add up to
+100%:
+
+``` r
+d %>%
+  mutate(alleles_over_2n = parse_number(alleles_over_2n)) %>%
+  group_by(population) %>%
+  summarize(sum = sum(alleles_over_2n)) %>%
+  count(sum == 1)
+```
+
+    ## # A tibble: 2 × 2
+    ##   `sum == 1`     n
+    ##   <lgl>      <int>
+    ## 1 FALSE        403
+    ## 2 TRUE          37
+
+Plot the frequency of a specific allele in populations with more than
+1000 sampled individuals:
+
+``` r
+d <- fread("allelefrequencies.net/hla/DQB1.tsv")
+my_allele <- "DQB1*02:01"
+my_d <- d %>% filter(allele == my_allele) %>%
+  mutate(
+    n = parse_number(n),
+    alleles_over_2n = parse_number(alleles_over_2n)
+  ) %>%
+  filter(n > 1000) %>%
+  arrange(-alleles_over_2n)
+
+ggplot(my_d) +
+  aes(x = alleles_over_2n, y = reorder(population, alleles_over_2n)) +
+  scale_y_discrete(position = "right") +
+  geom_colh() +
+  labs(
+    x = "Allele Frequency (Alleles / 2N)",
+    y = NULL,
+    title =  glue("Frequency of {my_allele} across populations"),
+    caption = "Data from AFND http://allelefrequencies.net"
+  )
+```
+
+![](README_files/figure-gfm/unnamed-chunk-6-1.png)<!-- -->
+
+## Citation
+
+If you use this data, please cite the latest manuscript about **Allele
+Frequency Net Database**:
+
+  - Gonzalez-Galarza FF, McCabe A, Santos EJMD, Jones J, Takeshita L,
+    Ortega-Rivera ND, et al. [Allele frequency net database (AFND) 2020
+    update: gold-standard data classification, open access genotype data
+    and new query tools.](https://pubmed.ncbi.nlm.nih.gov/31722398)
+    Nucleic Acids Res. 2020;48: D783–D788. <doi:10.1093/nar/gkz1029>
+
+Also, I want to say thanks to David A. Wells for sharing
+[scrapeAF](https://github.com/DAWells/scrapeAF), which inspired me to
+work on this project.
---
+++ allelefrequencies.py
@@ -0,0 +1,227 @@
+"""
+allelefrequencies.py
+====================
+
+Kamil Slowikowski
+2023-03-14
+
+
+Overview
+--------
+
+This script downloads allele frequencies for HLA, KIR, MIC, and cytokine genes
+from the Allele Frequency Net Database:
+
+http://allelefrequencies.net
+
+Then, the script writes .tsv files like this:
+
+    ls allelefrequencies.net/hla/
+        787K  A.tsv
+        1.5M  B.tsv
+        454K  C.tsv
+        8.5K  DPA1.tsv
+        201K  DPB1.tsv
+         88K  DQA1.tsv
+        291K  DQB1.tsv
+        1.2M  DRB1.tsv
+
+Each file looks like this:
+
+    head allelefrequencies.net/A.tsv
+
+    allele   population                               allele_freq  sample_size
+    A*01:01  Argentina Rosario Toba                   0.0760       86
+    A*01:01  Armenia combined Regions                 0.1250       100
+    A*01:01  Australia Cape York Peninsula Aborigine  0.0530       103
+    A*01:01  Australia Groote Eylandt Aborigine       0.0270       75
+    A*01:01  Australia New South Wales Caucasian      0.1870       134
+    A*01:01  Australia Yuendumu Aborigine             0.0080       191
+    A*01:01  Austria                                  0.1460       200
+    A*01:01  Azores Central Islands                   0.0800       59
+    A*01:01  Azores Oriental Islands                  0.1150       43
+
+
+Citation
+--------
+
+Please cite the latest manuscript about Allele Frequency Net Database:
+
+https://pubmed.ncbi.nlm.nih.gov/31722398
+
+Gonzalez-Galarza FF, McCabe A, Santos EJMD, Jones J, Takeshita L, Ortega-Rivera
+ND, et al. Allele frequency net database (AFND) 2020 update: gold-standard data
+classification, open access genotype data and new query tools. Nucleic Acids
+Res. 2020;48: D783–D788. doi:10.1093/nar/gkz1029
+
+
+Acknowledgments
+---------------
+
+Thanks to David A. Wells for sharing [scrapeAF][1], which inspired me to work
+on this project.
+
+[1]: https://github.com/DAWells/scrapeAF
+
+"""
+
+import os
+import pandas as pd
+import re
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+from tqdm import tqdm
+import gzip
+
+def main():
+    download_hla()
+    download_kir()
+    download_cyt()
+    download_mic()
+
+def download_hla():
+    out = 'hla'
+    base = 'http://www.allelefrequencies.net/hla6006a.asp'
+    loci = ['A', 'B', 'C', 'DPA1', 'DPB1', 'DQA1', 'DQB1', 'DRB1']
+    for locus in loci:
+        tsv_file = f'allelefrequencies.net/{out}/{locus}.tsv'
+        print(tsv_file)
+        if not os.path.exists(tsv_file):
+            params = {
+                'hla_locus': locus,
+                'hla_locus_type': 'Classical',
+                'hla_level': '2'
+            }
+            url = f'{base}?{urlencode(params)}'
+            df = get_all_pages(url)
+            df = df[[1,3,4,5,7]]
+            df.columns = ['allele', 'population',
+                    'indivs_over_n', 'alleles_over_2n', 'n']
+            write_tsv(df, tsv_file)
+
+def download_kir():
+    out = 'kir'
+    base = 'http://www.allelefrequencies.net/kir6002a.asp'
+    loci = ['2DL1', '2DL2', '2DL3', '2DL4', '2DL5', '2DL5A', '2DL5B', '2DP1',
+            '2DS1', '2DS2', '2DS3', '2DS4', '2DS5', '3DL1', '3DL2', '3DL3',
+            '3DP1', '3DS1']
+    for locus in loci:
+        tsv_file = f'allelefrequencies.net/{out}/{locus}.tsv'
+        print(tsv_file)
+        if not os.path.exists(tsv_file):
+            params = {
+                'kir_locus': locus
+            }
+            url = f'{base}?{urlencode(params)}'
+            df = get_all_pages(url)
+            df = df[[1,3,5,6,8]]
+            df.columns = ['allele', 'population',
+                    'indivs_over_n', 'alleles_over_2n', 'n']
+            write_tsv(df, tsv_file)
+
+def download_cyt():
+    out = 'cyt'
+    base = 'http://www.allelefrequencies.net/cyt6001a.asp'
+    loci = ['AIF-1/', 'bFGF/', 'EGF/', 'GM-CSF/', 'IFNgamma/', 'IGF-1/',
+            'IL-10/', 'IL-12/', 'IL-12p40/', 'IL-13/', 'IL-15/', 'IL-18/',
+            'IL-1alpha/', 'IL-1beta/', 'IL1RA/', 'IL1R/', 'IL-2/', 'IL-4/',
+            'IL-4R alpha/', 'IL-4R/', 'IL-6/', 'NGF/', 'PDGF A/', 'PDGF B/',
+            'RANTES/', 'TGFbeta1/', 'TNFalpha/', 'TNFbeta/', 'VEGF/']
+    for locus in loci:
+        tsv_file = f'allelefrequencies.net/{out}/{safe(locus)}.tsv'
+        print(tsv_file)
+        if not os.path.exists(tsv_file):
+            params = {
+                'cyt_gene': locus
+            }
+            url = f'{base}?{urlencode(params)}'
+            df = get_all_pages(url)
+            if not df.empty:
+                df = df[[1,3,4,5,6]]
+                df.columns = ['allele', 'population',
+                        'indivs_over_n', 'alleles_over_2n', 'n']
+                write_tsv(df, tsv_file)
+
+def download_mic():
+    out = 'mic'
+    base = 'http://www.allelefrequencies.net/mic6001a.asp'
+    loci = ['MICA', 'MICB']
+    for locus in loci:
+        tsv_file = f'allelefrequencies.net/{out}/{locus}.tsv'
+        print(tsv_file)
+        if not os.path.exists(tsv_file):
+            params = {
+                'mic_locus': locus,
+                'mic_locus_type': 'Classical',
+                'mic_order': 'order_1'
+            }
+            url = f'{base}?{urlencode(params)}'
+            df = get_all_pages(url)
+            df = df[[1,3,5,6,8]]
+            df.columns = ['allele', 'population',
+                    'indivs_over_n', 'alleles_over_2n', 'n']
+            write_tsv(df, tsv_file)
+
+def write_tsv(d, tsv_file):
+    print(f'Writing {tsv_file}')
+    os.makedirs(os.path.dirname(tsv_file), exist_ok=True)
+    d.to_csv(tsv_file, sep='\t', index=False)
+
+def safe(x):
+    return re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F ]", "-", x)
+
+def get_url(url):
+    if not os.path.exists('cache'):
+        os.makedirs('cache', exist_ok=True)
+    cache_file = f'cache/{safe(url)}.html.gz'
+    if os.path.exists(cache_file):
+        text = gzip.open(cache_file, 'rt').read()
+    else:
+        req = requests.get(url)
+        text = req.text
+        with gzip.open(cache_file, 'wt') as out:
+            out.write(text)
+    return text
+
+def get_all_pages(url):
+    text = get_url(url)
+    bs = BeautifulSoup(text, 'html.parser')
+    # Number of pages of results
+    n_pages = bs(text = re.compile(r' of \d'))
+    if n_pages:
+        n_pages = int(n_pages[0].strip()[3:].replace(',', ''))
+    else:
+        n_pages = 1
+    if bs(text = re.compile(r'we did not find any results')):
+        return pd.DataFrame(None)
+    print(f'{n_pages} pages of results')
+    tables = []
+    # for i in range(1, min(n_pages + 1, 4)):
+    for i in tqdm(range(1, n_pages + 1)):
+        text = get_url(f'{url}&page={i}')
+        bs = BeautifulSoup(text, 'html.parser')
+        table = get_df(bs)
+        tables.append(table)
+    df = pd.concat(tables)
+    # Drop columns that have exactly 1 unique value
+    # for col in df.columns[3:]:
+    #     if len(df[col].unique()) == 1:
+    #         df.drop(col, inplace=True, axis=1)
+    return df 
+
+def get_df(bs):
+    table = bs.find('table', {'class': 'tblNormal'})
+    trs = table.find_all('tr')
+    th = trs[0].find_all('th')
+    columns = [x.get_text(strip=True) for x in th]
+    rows = []
+    for tr in trs[1:]:
+        fields = [td.get_text(strip=True) for td in tr.find_all('td')]
+        rows.append(fields)
+    df = pd.DataFrame(rows)
+    return df
+
+if __name__ == '__main__':
+    main()
+
